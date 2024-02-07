b961d4fb5e6266c80c89293a3f38913e49d55c12
frantecbh <franciscomenezesbh@gmail.com>
2024-02-07 20:54:18 -0300

    criando redrect expo router

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,6 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams, useNavigation } from 'expo-router'
+import { Redirect, useLocalSearchParams, useNavigation } from 'expo-router'
 import { PRODUCTS } from '@/utils/data/products'
 import { formatCurrency } from '@/utils/functions/format-currency'
 import { Button } from '@/components/button'
@@ -20,18 +20,29 @@ const Product = () => {
 
 
 
-const product = PRODUCTS.filter((item) => item.id === id)[0]
+//const product = PRODUCTS.filter((item) => item.id === id)[0]
+const product = PRODUCTS.find((item) => item.id === id)
 console.log(carStore.products)
 
 function handleAddToCart(){
-  carStore.add(product)
-  navigation.goBack()
+
+  if (product){
+    carStore.add(product!)
+    navigation.goBack()
+  }
+ 
+}
+
+if(!product){
+  return <Redirect href="/" />
 }
 
   return (
     <View className='flex-1'>
      <Image source={product.cover} className='w-full h-52' resizeMode='cover' />
+     
      <View className='p-5 mt-8 flex-1'>
+     <Text className='text-white text-xl font-heading'>{product.title}</Text>
       <Text className='text-lime-400 text-2xl font-heading my-2'>{formatCurrency(product.price)}</Text>
       <Text className='text-slate-400 font-body text-base leading-6 mb-6'>{product.description}</Text>
 
