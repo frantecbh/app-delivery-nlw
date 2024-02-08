cfeffb5f1607f6bba0d4ebd68327f8b32a053a84
frantecbh <franciscomenezesbh@gmail.com>
2024-02-07 22:57:44 -0300

    asyncstorage cart

--- src/sotres/cart-store.ts
+++ src/sotres/cart-store.ts
@@ -1,6 +1,10 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 
+import {createJSONStorage, persist} from 'zustand/middleware'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import * as cartInMemory from './helpers/cart-in-memory'
 
 export type ProductCardProps = ProductProps & {
@@ -13,7 +17,8 @@ type StateProps = {
   remove: (productId: string) => void
 }
 
-export const userCardStore = create<StateProps>((set) => ({
+export const userCardStore = create(
+  persist<StateProps>((set) => ({
   products: [],
   add: (product: ProductProps) =>
   set((state) => ({
@@ -25,4 +30,7 @@ export const userCardStore = create<StateProps>((set) => ({
     products: cartInMemory.remove(state.products, productId)
   }))
   
+}),{
+name: "app-delivey:cart",
+storage: createJSONStorage(() => AsyncStorage)
 }))
